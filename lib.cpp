3c885870fd2266a45168d2594cd8efebbb2ebe0c
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-12-10 21:20:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+
+float funzione( ) {
+    int i,n;
+    float   max;
+    cin>>n;
+    float array[n];
+    for (i = 0; i < n; i++) {
+        cin >> array[i];
+    }
+    max = array[0];
+
+    for (i = 0; i < n; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
